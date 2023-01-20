772d99ce286c75e4994c37190c95d603de39ed84
Dung Van Huynh <dung1983huynh@gmail.com>
2023-01-20 14:43:56 +0900

    Header Icon margin

--- src/pages/common/dashboard/Header.js
+++ src/pages/common/dashboard/Header.js
@@ -25,10 +25,10 @@ const Mainhead = ({ menu, expanded, onToggleSidebar, authstate, logout }) => {
 
   return (
     <div className={getHeaderStyle()}>
-      <IconButton className="mt-2" onClick={() => onToggleSidebar()}>
+      <IconButton onClick={() => onToggleSidebar()}>
         <DrawIcon />
       </IconButton>
-      <div className="dashboard-header-title">
+      <div className="dashboard-header-title mb-2">
         <div>{currentItem ? currentItem.title : "Home"}</div>
       </div>
       {authstate === true ? (
